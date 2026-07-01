35e84c0f85d07bacc02fe4b0294b9eab76495ddb
Domisnnet <dominique.goncalves61@gmail.com>
2026-07-01 11:01:37 -0300

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root-component',
+  selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
