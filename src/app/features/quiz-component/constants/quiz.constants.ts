a60693604056489f1d109a2e96133b56bfa78339
Domisnnet <dominique.goncalves61@gmail.com>
2026-07-04 17:40:26 -0300

    feat: add CSS design variables, quiz constants, and result component styling

--- src/app/features/quiz-component/constants/quiz.constants.ts
+++ src/app/features/quiz-component/constants/quiz.constants.ts
@@ -3,5 +3,5 @@ import { QuizScore } from '@quiz/models/quiz-score.model';
 export const QUIZ_CONSTANTS = {
   INITIAL_QUESTION: 0,
   INITIAL_SCORE: <QuizScore>{ hero: 0, hench: 0 },
-  RESULT_THRESHOLDS: { HERO: 18, PROTECTOR: 8, VIGILANTE_MIN: -7, VIGILANTE_MAX: 7, MERCENARY: -18 }
+  RESULT_THRESHOLDS: { HERO: 17, PROTECTOR: 8, VIGILANTE_MIN: -7, VIGILANTE_MAX: 7, MERCENARY: -17 }
 } as const;
