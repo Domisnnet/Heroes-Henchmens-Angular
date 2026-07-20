5f043c8f1393bf67ef660a01aa25bbbf97411311
Domisnnet <dominique.goncalves61@gmail.com>
2026-07-20 15:11:30 -0300

    REFATORADO

--- src/app/features/codex/codex-page/codex-page.ts
+++ src/app/features/codex/codex-page/codex-page.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CodexComponent } from '@shared/ui/codex';
 import { CODEX_RECORDS } from '@shared/data/codex/codex.data';
-
+import { CodexComponent } from '@shared/ui/codex';
 @Component({
   selector: 'app-codex-page',
   standalone: true,
-  imports: [ CodexComponent ],
+  imports: [CodexComponent],
   templateUrl: './codex-page.html',
   styleUrls: ['./codex-page.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
