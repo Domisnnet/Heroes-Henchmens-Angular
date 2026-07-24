2e3a16b477dfb5498c5fc13d5c64e70f3e0ec37c
Domisnnet <dominique.goncalves61@gmail.com>
2026-07-24 11:03:42 -0300

    refactor: condense eslint configuration formatting for readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,19 +7,14 @@ export default [
   {
     files: ['**/*.ts'],
     languageOptions: {
-      parserOptions: {
-        project: ['./tsconfig.json'],
-      },
+      parserOptions: { project: ['./tsconfig.json'] },
     },
     rules: {
       'no-console': 'warn',
       '@typescript-eslint/consistent-type-imports': 'error',
       '@typescript-eslint/no-unused-vars': [
         'error',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-        },
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
     },
   },
