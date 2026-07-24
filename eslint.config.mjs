c39c76d6d7a8ce399a71c12282f991e3260c1ddc
Domisnnet <dominique.goncalves61@gmail.com>
2026-07-24 11:14:59 -0300

    chore: update ESLint configuration and dependencies to include Prettier integration and improved linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,25 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
 export default [
+  { ignores: [ '.angular/**', 'coverage/**', 'dist/**', 'node_modules/**' ] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
     files: ['**/*.ts'],
     languageOptions: {
-      parserOptions: { project: ['./tsconfig.json'] },
+      parserOptions: { project: './tsconfig.json' },
     },
     rules: {
       'no-console': 'warn',
+      'no-debugger': 'error',
       '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
     },
-  },
-  { ignores: ['dist/**', 'coverage/**', 'node_modules/**'] },
+  }, prettier
 ];
